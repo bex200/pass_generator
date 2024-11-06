f1978621953db2410f035be845005057a8fff921
fedenka123 <fedor_popoff@mail.ru>
2024-11-06 15:44:02 +0500

    revised generator

--- password_generator.py
+++ password_generator.py
@@ -4,6 +4,7 @@ import string
 import nltk
 from nltk.corpus import words
 
+
 # Узнаем длину генерируемого пароля
 def get_password_len():
     while True:
@@ -19,18 +20,18 @@ def get_password_len():
         except ValueError:
             print("ERROR: Введите целое число от 6 до 12 включительно:\n")
 
-
+    
 
 # Узнаем какой уровень сложности хочет пользователь для своего пароля
 def get_level_of_password():
     try:
-        level_of_password = int(input("""Введите уровень сложности пароля:
+        (lvl_of_pass) = int(input("""Введите уровень сложности пароля:
                          1 - Легкий
                          2 - Средний
                          3 - Сложный\n"""))
-        if level_of_password in(1,2,3):
+        if (lvl_of_pass) in(1,2,3):
             print("Принято!")
-            return(level_of_password)
+            return((lvl_of_pass))
         else: 
             print("""Не ввел верно ;) Получил пароль сложный, удачи запомнить""")
             return 4
@@ -44,7 +45,7 @@ def get_level_of_password():
 
 
 # Генератор пароля
-def get_password(level_password, password_len):
+def get_password(lvl_pass, pass_len):
     nltk.download("words")  # Грузим слова из библиотеки
     word_list = []
     digit_list = string.digits
@@ -52,49 +53,49 @@ def get_password(level_password, password_len):
     word_symbol_digit_list = string.ascii_letters + string.digits + string.punctuation
     
     # Блок для 1 уровня сложности пароля
-    if level_password == 1:
+    if lvl_pass == 1:
         for word in words.words():
-            if len(word) == password_len:
+            if len(word) == pass_len:
                 word_list.append(word)
         return random.choice(word_list)
 
     # Блок для 2 уровня сложности пароля
-    elif level_password == 2:
+    elif lvl_pass == 2:
         # Создаю рандомное количество цифр в конце слова... -1 делаю что бы пароль не состоял из одних цифр
-        random_amount_of_digits = random.randint(1, password_len-1)         
+        rnd_digits = random.randint(1, pass_len-1)         
         for word in words.words():
             # проверяю длину найденного слова что бы не превышало запрашиваемое(в общем объеме)
-            if len(word) == password_len - random_amount_of_digits:
+            if len(word) == pass_len - rnd_digits:
                 word_list.append(word)
-        return random.choice(word_list) + ''.join(random.choices(digit_list, k=random_amount_of_digits))
+        return random.choice(word_list) + ''.join(random.choices(digit_list, k=rnd_digits))
     
     # Блок для 3 уровня сложности пароля
-    elif level_password == 3:
+    elif lvl_pass == 3:
         # Создаю рандомное количество цифр в конце слова... -1 делаю что бы пароль не состоял из одних цифр
-        random_amount_of_digits = random.randint(1, password_len-1)
+        rnd_digits = random.randint(1, pass_len-1)
         # Создаю рандомное количество символов в конце слова... -1 делаю что бы пароль не состоял из одних цифр или символов
-        random_amount_of_symbols = random.randint(1,password_len - random_amount_of_digits - 1)
+        rnd_symbol = random.randint(1,pass_len - rnd_digits - 1)
         for word in words.words():
             # проверяю длину найденного слова что бы не превышало запрашиваемое(в общем объеме)
-            if len(word) == password_len- random_amount_of_digits - random_amount_of_symbols:
+            if len(word) == pass_len- rnd_digits - rnd_symbol:
                 word_list.append(word)
-        return random.choice(word_list) + ''.join(random.choices(digit_list, k=random_amount_of_digits)) + ''.join(random.choices(symbol_list, k=random_amount_of_symbols))
+        return random.choice(word_list) + ''.join(random.choices(digit_list, k=rnd_digits)) + ''.join(random.choices(symbol_list, k=rnd_symbol))
     
     # Блок для пароля из набора букв и символов
-    elif level_password not in [1,2,3]:
+    elif lvl_pass not in [1,2,3]:
         password = ""
-        for word in range(password_len):
+        for word in range(pass_len):
             password += random.choice(word_symbol_digit_list)
         return password
     
  
-def save_password(password_ready): 
+def save_password(pass_ready): 
     website = input("Введите пароль для какой платформы:\n")
     with open("passwords.txt", "a", encoding="utf-8") as file:
         # сохраняет пароль в файл
-        file.write(f"{website} ---- {password_ready} ---- {datetime.now().strftime('%b-%d-%Y %H:%M')}\n")      
+        file.write(f"{website} ---- {pass_ready} ---- {datetime.now().strftime('%b-%d-%Y %H:%M')}\n")      
         # выводит сайт-пароль-время в терминал                                  
-        return (f"Ваша запись сохранена:\n{website} ---- {password_ready} ---- {datetime.now().strftime('%b-%d-%Y %H:%M')}\n")          
+        return (f"Ваша запись сохранена:\n{website} ---- {pass_ready} ---- {datetime.now().strftime('%b-%d-%Y %H:%M')}\n")          
     
 def main():
     password = get_password_len()                       # Функция для ввода длины пароля
