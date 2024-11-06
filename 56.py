c2e83cb360e3775e2c9fb9c92cb02069374960bb
Мажен Алтынай <amazhen@paymart.local>
2024-11-06 19:22:27 +0500

    All files are added

---
+++ 56.py
@@ -0,0 +1,10 @@
+first = int(input("Введите первое число: ")) 
+second = int(input("Введите второе число: ")) 
+total = first + second
+print("Sum of two numbers is equal to", total) 
+multiply = first*second 
+print("Multiplication of two numbers is equal to:", multiply) 
+if first >= second:
+    print("First number is equal or greater than second number")
+    else:
+    print("First number is less than second number")
