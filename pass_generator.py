49dda5937b9bb85472c851fdf57f5a3af74805b3
akadyrov <aydosk2002@yahoo.com>
2024-11-12 10:26:30 +0500

    доработана логика выбора символа. доработан формат вывода времени

--- pass_generator.py
+++ pass_generator.py
@@ -22,12 +22,40 @@ def is_special_symbol():
 
 
 def generate_pass(pass_len, is_any_symbol):
-    list_of_elements = list(string.ascii_letters) + list(string.digits)
+    list_of_lower_letters = list(string.ascii_lowercase)
+    list_of_upper_letters = list(string.ascii_uppercase)
+    list_of_digits = list(string.digits)
+    pass_settings = {
+        0: list_of_lower_letters,
+        1: list_of_upper_letters,
+        2: list_of_digits,
+    }
+    pass_stamp = []
+
     if is_any_symbol:
-        list_of_elements.extend(string.punctuation)
+        list_of_punctuation = string.punctuation
+        pass_settings[3] = list_of_punctuation
     generated_pass = ""
-    for element in range(pass_len):
-        generated_pass += random.choice(list(list_of_elements))
+
+    while True:
+        pass_stamp = []
+        for each in range(pass_len):
+            pass_stamp.append(random.choice(list(pass_settings.keys())))
+
+        if (
+            pass_stamp.count(0) == 0
+            or pass_stamp.count(1) == 0
+            or pass_stamp.count(2) == 0
+        ):
+            continue
+        elif is_any_symbol and max(pass_stamp) != 3:
+            continue
+        else:
+            break
+
+    for i in range(len(pass_stamp)):
+        generated_pass += random.choice(pass_settings.get(pass_stamp[i]))
+
     return generated_pass
 
 
@@ -39,9 +67,10 @@ def save_pass(final_pass):
         date_now = datetime.datetime.now()
 
         with open("pass.txt", "a", encoding="utf-8") as file:
-            result = f"{webservice} ------ {final_pass} -------- {str(date_now)}\n"
+            result = f"{webservice} ------ {final_pass} -------- {str(date_now.strftime("%d.%m.%Y, %H:%M"))}\n"
             file.write(result)
-    return
+    else:
+        print(final_pass)
 
 
 def main():
